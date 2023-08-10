278427ed4775bfeeb102c72650daadb8345c8dea
Amir <sammasovamir29@gmail.com>
2023-08-11 01:39:57 +0300

    Final beta

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"time"
+)
+
+type Film struct {
+	Title    string
+	Director string
+}
 
 func main() {
-	fmt.Println("test")
+	fmt.Println("server started at port 8000")
+
+	homeHandler := func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("index.html"))
+
+		films := map[string][]Film{
+			"Films": {
+				{Title: "The Godfather", Director: "Francis Ford Coppola"},
+				{Title: "Blade Runner", Director: "Ridley Scott"},
+				{Title: "Gladiator", Director: "Russel Crow"},
+			},
+		}
+
+		tmpl.Execute(w, films)
+	}
+
+	createHandler := func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(1 * time.Second)
+		title := r.PostFormValue("title")
+		director := r.PostFormValue("director")
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		tmpl.ExecuteTemplate(w, "film-list-element", Film{Title: title, Director: director})
+	}
+
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/add-film/", createHandler)
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
